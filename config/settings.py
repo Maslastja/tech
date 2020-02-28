27fe0deb8d734d15d0b116fa907e94c3da324751
Anastasiya <maslastja@gmail.com>
2020-02-28 09:29:33 +0800

    change settings

--- config/settings.py
+++ config/settings.py
@@ -1,12 +1,9 @@
 import os
 
-DATABASE = os.getenv('DATABASE_URL')
+DATABASE = 'sqliteext:///db_techpage.sqlite'
+DATABASE_URL = 'sqliteext:///db_techpage.sqlite'
 SECRET_KEY = os.urandom(24)
 SESSION_COOKIE_NAME = 'sid'
 LOGIN_URL = 'http://auth.iood.ru/user/login?next={}'
 SESSION_GET = 'http://auth.iood.ru/sess/get'
 SESSION_STORE = 'http://auth.iood.ru/sess/update'
-
-PHONE_IRK = 'smb://192.168.109.20/Common Linux/ATC/телефоны 2019.ods'
-PHONE_ANG = 'smb://192.168.109.20/Common Linux/ATC/телефоны Ангарска.xls'
-PHONE_BR = 'smb://192.168.109.20/Common Linux/ATC/телефоны Братск1.xls'
--- config/settings_test.py
+++ config/settings_test.py
@@ -2,4 +2,5 @@ SESSION_COOKIE_NAME = 'sid'
 SESSION_GET = 'http://auth.iood.ru/sess/get'
 SESSION_STORE = 'http://auth.iood.ru/sess/update'
 SERVER_NAME = 'tech.iood.ru'
+AUTH = 'http://auth.iood.ru/user/login'
 TESTING = True
