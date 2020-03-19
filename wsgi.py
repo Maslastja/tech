b3112f755c9d08621d03fb2e12d780c11fc54dc6
Anastasiya <maslastja@gmail.com>
2020-03-19 14:33:54 +0800

    com wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,9 @@
 from app.core import create_app
 
 app = create_app()
-if app.debug:
-    from app.commands import dbase
-    app.cli.add_command(dbase)
+# if app.debug:
+#    from app.commands import dbase
+#    app.cli.add_command(dbase)
 
-if __name__ == '__main__':
-    app.run(host='tech.iood.ru')
+# if __name__ == '__main__':
+#    app.run(host='tech.iood.ru')
